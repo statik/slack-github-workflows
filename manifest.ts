66e0e338303cb37e579a1b896e9590ad2ad58a18
Elliot Murphy <statik@users.noreply.github.com>
2022-09-30 12:24:48 -0400

    initial template
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

---
+++ manifest.ts
@@ -0,0 +1,16 @@
+import { Manifest } from "deno-slack-sdk/mod.ts";
+
+/**
+ * The app manifest contains the app's configuration. This
+ * file defines attributes like app name and description.
+ * https://api.slack.com/future/manifest
+ */
+export default Manifest({
+  name: "slack-github-workflows",
+  description: "A blank template for building Slack apps with Deno",
+  icon: "assets/icon.png",
+  functions: [],
+  workflows: [],
+  outgoingDomains: [],
+  botScopes: ["commands", "chat:write", "chat:write.public"],
+});
